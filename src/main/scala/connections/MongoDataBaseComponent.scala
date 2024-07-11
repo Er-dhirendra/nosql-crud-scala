b5bc38b3ed5360794eec120c08413a6d6803bc0d
dkashyap1090 <dkashyap1090@outlook.com>
2024-07-11 08:07:56 +0530

    Initial commit

--- src/main/scala/connections/MongoDataBaseComponent.scala
+++ src/main/scala/connections/MongoDataBaseComponent.scala
@@ -7,5 +7,5 @@ import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
 trait MongoDataBaseComponent extends MongoComponent {
   val mongoClient: MongoClient = MongoClient()
   val database: MongoDatabase = mongoClient.getDatabase("mydb").withCodecRegistry(codecRegistry)
-  val collection: MongoCollection[Person] = database.getCollection("test")
+  val collection: MongoCollection[Person] = database.getCollection("mycollection")
 }
